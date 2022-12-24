e8f4235ed49e9c9cdfc554b147be2d1ee11855af
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-24 16:49:00 +0600

    add header-body layout in header

--- src/app/components/common/Header/Header.tsx
+++ src/app/components/common/Header/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import NavList from "../NavList";
-import {navigationRoutes} from "../../../router/routes";
+import {navigationRoutes, productsNavigationRoutes} from "../../../router/routes";
 import HeaderTop from "./HeaderTop";
+import HeaderBody from "./HeaderBody";
 
 const Header: React.FC = () => {
     return (
         <header className="header">
             <div className="header__wrapper lock-padding">
                 <HeaderTop links={navigationRoutes}/>
+                <HeaderBody links={productsNavigationRoutes}/>
             </div>
         </header>
     );
--- src/app/components/common/Header/index.ts
+++ src/app/components/common/Header/index.ts
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import HeaderTop from "./HeaderTop";
+import HeaderBody from "./HeaderBody";
 
-export { HeaderTop }
+export { HeaderTop, HeaderBody }
 export default Header;
