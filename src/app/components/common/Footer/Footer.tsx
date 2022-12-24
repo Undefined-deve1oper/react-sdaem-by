528d5915cdf26477e79c38cc391037187ae5d4bf
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-24 21:50:54 +0600

    preparing to write footer

---
+++ src/app/components/common/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container _container">
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
