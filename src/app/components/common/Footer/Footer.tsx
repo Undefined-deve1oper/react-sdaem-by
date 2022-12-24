e58e3faa55e8e645b59aeb5acd8ab5898b24c099
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-24 21:55:29 +0600

    add footer-body layout in footer

--- src/app/components/common/Footer/Footer.tsx
+++ src/app/components/common/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FooterBody from "./FooterBody";
 
-const Footer = () => {
+const Footer: React.FC = () => {
     return (
         <footer className="footer">
             <div className="footer__container _container">
+                <FooterBody/>
             </div>
         </footer>
     );
