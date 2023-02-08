15cf1f5a12054c47819227f4d1896ee533b3506a
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-08 20:20:10 +0600

    added the functionality of adding to the database

--- server/startUp/initDatabase.js
+++ server/startUp/initDatabase.js
@@ -11,6 +11,21 @@ module.exports = async () => {
 
 async function createInitialEntity(Model, data) {
     await Model.collection.drop();
+    if (!Array.isArray(data)) {
+        return Promise.all(
+            Object.keys(data).map(async (item) => {
+                try {
+                    delete data[item]._id;
+                    const newItem = new Model(data[item]);
+                    await newItem.save();
+                    return newItem;
+                } catch (error) {
+                    return error;
+                }
+            })
+        );
+    }
+
     return Promise.all(
         data.map(async (item) => {
             try {
