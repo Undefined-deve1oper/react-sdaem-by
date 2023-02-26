5f1c1c1324434f4c7161e128fe32e7f449788a5a
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-26 23:47:42 +0600

    remove UserBooking

--- client/src/app/layouts/UserBooking.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BookingPage from "../components/pages/BookingPage";
-
-const UserBooking: React.FC = () => {
-    return <BookingPage />;
-};
-
-export default UserBooking;
