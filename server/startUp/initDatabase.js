5e4d84579f1b2dbd3cf5d87027eb4574620f54f0
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-26 20:01:28 +0600

    edit in initDatabase

--- server/startUp/initDatabase.js
+++ server/startUp/initDatabase.js
@@ -36,7 +36,7 @@ module.exports = async () => {
     }
 
     const estates = await Estate.find();
-    if (true || estates.length !== estatesMockData.length) {
+    if (estates.length !== estatesMockData.length) {
         await createInitialEntity(Estate, estatesMockData);
         console.log("added estate in mongoDB");
     }
